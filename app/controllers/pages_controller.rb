4b71d68dd748679781902e6d8fac73e99cfc5c4e
Eddie Zaneski <eddiezane@gmail.com>
2013-03-31 03:16:06 -0400

    Add pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Biographer::Application.routes.draw do
+  root to: 'pages#index'
+  get "pages/index"
+
   devise_for :users
 end
