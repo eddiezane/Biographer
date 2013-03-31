f3fac0827c17df1ad7cd7c0e6b011e34a3595a3d
Eddie Zaneski <eddiezane@gmail.com>
2013-03-31 01:15:03 -0400

    Base init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biographer::Application.config.secret_token = '8d210d484b95f15c542755d7ff24179e782ccf1c333d5a2804589a5bbf687cc9fa36b5aa8e48e43c911d03575db19d4727b85a0487ef7c9fb61e8f207389879c'
